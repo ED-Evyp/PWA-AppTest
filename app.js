15b5be8614ea84cd8a7161128faf6575d3974ff4
ED-Evyp <ed.evyp@gmail.com>
2026-02-05 13:57:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Register service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/my-first-pwa/service-worker.js')
+    navigator.serviceWorker.register('/PWA-AppTest/service-worker.js')
         .then(() => console.log('Service Worker registered!'))
         .catch(err => console.log('Service Worker registration failed:', err));
 }
